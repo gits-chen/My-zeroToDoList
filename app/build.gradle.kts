cc802c2608d8ee89275d26ac8b6ec531494e0b9d
misakanee <3460514978@qq.com>
2024-03-18 22:29:13 +0800

    3/18 晚 导入MaterialButton控件 创建了MaterialButtonToggleGroup组件并进行了调整（调整未完成） 系统的学习LinearLayout,RelativeLayout,FrameLayout之间的区别联系

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,7 +40,11 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
+    implementation("com.google.android.material:material:1.5.0")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
 }
