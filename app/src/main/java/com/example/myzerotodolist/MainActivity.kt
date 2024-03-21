8b2af3f198795d7d19db1c220c12dc26e8811c88
misakanee <3460514978@qq.com>
2024-03-21 21:49:31 +0800

    [200~3.19 晚 完善了MaterButtonToggleGroup组件，创建了三个Button类，分别是“笔记”“待办”“我的”接下来的工作就是向里面填充逻辑 学习了Toast功能，在程序里实现 学习了Activity间的转换，同时也了解一部分通过uri转至网页的方法，但试着将“我的”选项链接至原神官网时出现了跳转至浏览器后没有进入网页的bug，目前查阅资料后并未发现原因，待后来再探索 调整了我的小米手机使其能链接至AndroidStudio，故可以用实体机测试，摆脱了虚拟机慢的要死的问题

--- app/src/main/java/com/example/myzerotodolist/MainActivity.kt
+++ app/src/main/java/com/example/myzerotodolist/MainActivity.kt
@@ -1,10 +1,16 @@
 package com.example.myzerotodolist
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.myzerotodolist.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,5 +23,23 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        val contentView = binding.root
+        setContentView(contentView)
+
+        binding.notebook.setOnClickListener {
+            Toast.makeText(this,"你点了笔记",Toast.LENGTH_SHORT).show()
+        }
+        binding.list.setOnClickListener {
+            Toast.makeText(this,"你点了待办",Toast.LENGTH_SHORT).show()
+        }
+        binding.mine.setOnClickListener {
+            val uri = Uri.parse("https://ys.mihoyo.com/")
+            val intent = Intent()
+
+            intent.data = uri
+
+            startActivity(intent)
+        }
     }
 }
